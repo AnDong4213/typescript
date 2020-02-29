872656edaeb86bef92b2ed4fc64134da867e1160
赵安东 <zhaoandong4213@sina.com>
2020-02-29 13:23:22 +0800

    20/02/29

--- ts-axios/src/index.ts
+++ ts-axios/src/index.ts
@@ -1,5 +1,8 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
+import { transformRequest, transformResponse } from './helpers/data'
+import { processHeaders } from './helpers/headers'
 
 function axios(config: AxiosRequestConfig): void {
   xhr(config)
