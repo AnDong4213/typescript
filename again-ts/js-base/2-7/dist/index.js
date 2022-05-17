f433b31753a37f6284ab30222ff6f08bf2ce3745
赵安东 <zhaoandong4213@sina.com>
2022-05-17 21:54:41 +0800

    hulue

--- again-ts/js-base/2-7/dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-console.log("abcd");
-let count = 9299;
-console.log(count);
--- again-ts/js-base/2-7/dist/shop/shop.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-console.log("kk");
-let count = 2;
-console.log(count);
