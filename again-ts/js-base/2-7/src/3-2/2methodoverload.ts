a0bd60233d9f417804ef28aac3b5460265637ac2
赵安东 <zhaoandong4213@sina.com>
2022-05-22 21:44:35 +0800

    函数重载

--- again-ts/js-base/2-7/src/3-2/2methodoverload.ts
+++ again-ts/js-base/2-7/src/3-2/2methodoverload.ts
@@ -32,19 +32,30 @@ const messages: Array<Message> = [
   }
 ];
 
+// 函数重载
+// 重载签名的参数的类型一定是具体的
+function getMessage(value: number): Message;
+function getMessage(value: MessageType, count: number): Array<Message>;
+
+// 实现签名的参数的可以是any
 function getMessage(
-  value: number | MessageType
+  value: number | MessageType, // value可以是any
+  count: number = 1 // 没有初始值的话，第一个重载函数会报错
 ): Message | Array<Message> | undefined {
+  // 返回值的类型可以不写，会自动推导
   if (typeof value === "number") {
     return messages.find((msg) => value === msg.id);
   } else {
-    return messages.filter((msg) => value === msg.type);
+    // return messages.filter((msg) => value === msg.type);
+    return messages.filter((msg) => value === msg.type).splice(0, count);
   }
 }
 
-console.log(getMessage("audio"));
+/* console.log(getMessage("audio"));
 // let msg = getMessage(1).id; // 写法错误
 let msg = (<Message>getMessage(1)).id; // 类型转换，转换为联合类型中的(Message | Array<Message> | undefined)某一种类型
-console.log(msg);
-
-export {};
+console.log(msg); */
+console.log(getMessage(1).type);
+getMessage("image", 2).forEach((msg) => {
+  console.log(msg);
+});
