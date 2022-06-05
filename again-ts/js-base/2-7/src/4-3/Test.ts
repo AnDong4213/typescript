62339bae88a77b3150f11e1cf5a3300b9f75aef9
赵安东 <zhaoandong4213@sina.com>
2022-06-05 18:18:00 +0800

    feat: 一个静态方法改变了某个静态属性，其他静态方法或类外部任何地方访问这个属性都会发生改变

--- again-ts/js-base/2-7/src/4-3/Test.ts
+++ again-ts/js-base/2-7/src/4-3/Test.ts
@@ -1,4 +1,8 @@
 import MyLocalStorage from "./Mylocalstorage";
+MyLocalStorage.getInstance();
+MyLocalStorage.getInstance();
 
-const my = new MyLocalStorage();
-my.setItem("age", 23);
+// const my = new MyLocalStorage();
+// my.setItem("age", 23);
+MyLocalStorage.addTotal();
+console.log(MyLocalStorage.getTotal());
