a2f156567367580861b08981f1f225da884cfcf7
赵安东 <zhaoandong4213@sina.com>
2022-06-13 22:18:02 +0800

    feat: 实例

--- again-ts/5-inherit/src/5-9-1.js
+++ again-ts/5-inherit/src/5-9-1.js
@@ -28,7 +28,7 @@ ChinesePeople.prototype.constructor = ChinesePeople;
 const p = new ChinesePeople("小米", "1", "124", "中国2"); */
 
 // 封装
-function _extends(parent, son) {
+/* function _extends(parent, son) {
   // 第一步: 创建一个寄生构造函数
   function Middle() {
     this.constructor = son;
@@ -38,6 +38,59 @@ function _extends(parent, son) {
   return new Middle();
 }
 ChinesePeople.prototype = _extends(People, ChinesePeople);
-const p = new ChinesePeople("小米plus", "1", "124", "中国2");
+const p = new ChinesePeople("小米plus", "1", "124", "中国2"); */
+
+ChinesePeople.prototype = Object.create(People.prototype)
+ChinesePeople.prototype.constructor = ChinesePeople
+const p = new ChinesePeople("小米note", "1", "124", "中国2");
+
 console.log(p);
 p.doEat();
+
+
+// Object.create() 方法创建一个新对象，使用现有的对象来提供新创建的对象的 __proto__。
+const person = {
+  isHuman: false,
+  printIntroduction: function () {
+    console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
+  }
+};
+const me = Object.create(person, {
+  age: {
+    value: 25,
+    writable: true
+  }
+});
+console.log(me);
+console.log(me.__proto__); // {isHuman: false, printIntroduction: ƒ}
+me.name = "华为";
+me.isHuman = true;
+me.printIntroduction();
+console.log(me);
+
+/* const pe = {};
+console.log(pe.__proto__); // 有值
+console.log(pe.prototype); // undefined，没有值
+function aa() {}
+console.log('aa.__proto__', aa.__proto__) // 有值  */
+
+function Shape() {
+  this.x = 0;
+  this.y = 0;
+}
+Shape.prototype.move = function (x, y) {
+  this.x += x;
+  this.y += y;
+  console.info("Shape moved.");
+};
+
+function Rectangle() {
+  Shape.call(this); // call super constructor.
+}
+Rectangle.prototype = Object.create(Shape.prototype);
+Rectangle.prototype.constructor = Rectangle;
+
+const rect = new Rectangle();
+rect.move(1, 1);
+
+console.log(Object.prototype);
