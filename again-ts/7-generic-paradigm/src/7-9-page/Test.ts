b9df9fe63c8c7b3755173a5b97e17d6cfed2a9d8
赵安东 <zhaoandong4213@sina.com>
2022-06-30 22:04:08 +0800

    feat: 泛型约束

--- again-ts/7-generic-paradigm/src/7-9-page/Test.ts
+++ again-ts/7-generic-paradigm/src/7-9-page/Test.ts
@@ -1,8 +1,14 @@
-import Pager from "./pager";
-import { FoodDao } from "./dao";
 
-let pager = new Pager(3);
+import { FoodDao } from "./dao";
+import {Food} from './entity'
+
+let pager = new Pager<Food>(3);
+import Pager from "./pager";
 let foodDao = new FoodDao();
 let allFoods = foodDao.findAllFoods();
 pager.dataList = allFoods;
-console.log(pager.showCurrentPageData())
+
+// let dd = pager.showCurrentPageData()
+pager.showCurrentPageData().forEach(food => {
+    console.log(food.shop)
+})
--- again-ts/7-generic-paradigm/src/7-9-page/pager.ts
+++ again-ts/7-generic-paradigm/src/7-9-page/pager.ts
@@ -1,10 +1,10 @@
 import ArrayList from "./ArrayList";
 
-export default class Pager {
+export default class Pager<U extends object> {
   firstRecordNoCurPage!: number; //每一页的第一条记录号是多少
   pageSize: number = 3; //每一页总共有几条记录
   pageCount: number = 0; // 当前是第几页--从前端页面传递过来的数据
-  dataList!: ArrayList; // 封装数据表取出来的全部数据的集合类【等外部传递数据给dataList】
+  dataList!: ArrayList<U>; // 封装数据表取出来的全部数据的集合类【等外部传递数据给dataList】
 
   constructor(pageCount: number) {
     this.pageCount = pageCount;
