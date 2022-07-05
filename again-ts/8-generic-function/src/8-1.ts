4332e6ed2d69f2f37c9fc9546d580f4d7d7431b1
赵安东 <zhaoandong4213@sina.com>
2022-07-05 18:40:31 +0800

    fix: 排序

--- again-ts/8-generic-function/src/8-1.ts
+++ again-ts/8-generic-function/src/8-1.ts
@@ -1,26 +1,65 @@
-function quickSort<T extends number>(arr: Array<T>): Array<T> {
-    if (arr.length < 2) { return arr }
-  
-    var left: Array<T> = [];
-    var right: Array<T> = [];
-    // var mid = arr.splice(arr.length / 2 >> 1, 1)[0];
-    var mid = arr.splice(arr.length >> 1, 1)[0];
-    console.log("mid:", mid)
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i] < mid) {
-        left.push(arr[i]);
-      } else {
-        right.push(arr[i])
-      }
-    }
-    return quickSort(left).concat(mid, quickSort(right))
+function quickSort<T>(ar: Array<T>): Array<T> {
+  let arr = ar.slice();
+  if (arr.length < 2) {
+    return arr;
   }
-  
-  
-  var chineseNumArr = [3, 1, 8, 20, 923, 15, 2, 7, 13, 11, 19, 181,453, 5, 6, 17, 4, 1060];
-  
-  let chineseArrSort = quickSort(chineseNumArr)
-  console.log("chineseArrSort2:", chineseArrSort)
-  chineseArrSort.forEach(n => {
-    // console.log(n.toFixed(2))
-  })
+
+  var left: Array<T> = [];
+  var right: Array<T> = [];
+  // var mid = arr.splice(Math.floor(arr.length / 2), 1)[0];
+  // var mid = arr.splice(arr.length / 2 >> 1, 1)[0];
+  var mid = arr.splice(arr.length >> 1, 1)[0];
+  // console.log("mid:", mid);
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] < mid) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+  return quickSort(left).concat(mid, quickSort(right));
+}
+
+var chineseNumArr = [
+  3, 1, 8, 20, 923, 15, 2, 7, 13, 11, 19, 181, 453, 5, 6, 17, 4, 106
+];
+
+let numberArrSort = quickSort(chineseNumArr);
+console.log("numberArrSort2:", numberArrSort);
+/* console.log(
+  "numberArrSort2:",
+  chineseNumArr.sort((a, b) => a - b)
+); */
+
+// 中文排序
+function sortChinese<T>(arr: Array<T>): T[] {
+  return arr.sort((c1, c2) => (c1 as any).localeCompare(c2, "zh-CN"));
+}
+/* var chineseArr = ["武汉", "郑州", "太原", "济南", "沈阳", "大连", "鹅城"];
+console.log("numberArrSort2:", sortChinese(chineseArr)); */
+
+// (2). 字符串自排序
+function strSelfSort(str: string): string {
+  return quickSort(str.split("")).join("");
+}
+/* console.log(strSelfSort("chinese")); */
+
+// 判断数组中是否有中文元素
+function isChinese<T>(arr: Array<T>): boolean {
+  const pattern = /[\u4e00-\u9fa5]+/g;
+  return arr.some((str) => pattern.test(str as any));
+}
+console.log(isChinese(["kkk", "kkh看"]));
+
+// 中文+英文、数字数组排序混合方法
+function sort<T>(arr: Array<T>): T[] {
+  if (isChinese(arr)) {
+    //如果是中文数组
+    return sortChinese(arr);
+  }
+  let newArr = arr.map((item) => {
+    return typeof item === "string" ? strSelfSort(item) : item;
+  });
+  //英文、数字数组排序
+  return quickSort(newArr as any);
+}
