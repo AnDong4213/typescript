cc3c1c1328483193844a9ce3f0e4eb020927d335
赵安东 <zhaoandong4213@sina.com>
2022-07-07 15:24:05 +0800

    feat: ref

--- again-ts/vt/src/views/HomeView.vue
+++ again-ts/vt/src/views/HomeView.vue
@@ -1,16 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <HelloWorld :msg="strRef" />
+    <p ref="km" style="color: #42b983">开门</p>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, onMounted, isRef } from "vue";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default defineComponent({
   name: "HomeView",
+  setup() {
+    const strRef = ref<string>("911");
+    type objType = {
+      age: number;
+      sex: string;
+    };
+    const obj: objType = {
+      age: 3,
+      sex: "0",
+    };
+    const objRef = ref<objType>(obj);
+    console.log(strRef.value);
+    console.log(objRef.value.sex);
+    console.log(isRef(strRef));
+    const km = ref();
+    onMounted(() => {
+      console.log(km.value);
+    });
+
+    return {
+      strRef,
+      km,
+    };
+  },
   components: {
     HelloWorld,
   },
