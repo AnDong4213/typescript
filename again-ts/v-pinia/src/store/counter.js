313dc059da6357230d74b994af25369d494c6408
赵 安东 <zhaoandong@bobfintech.com.cn>
2022-07-21 18:28:02 +0800

    持久化vuex

--- again-ts/v-pinia/src/store/counter.js
+++ again-ts/v-pinia/src/store/counter.js
@@ -2,10 +2,20 @@
 import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', {
-  state: () => ({ count: 0 }),
+  state: () => ({ count: 0, name: '安东' }),
   actions: {
     increment() {
       this.count++
+    },
+  },
+  // persist: ['count'],
+  persist: {
+    paths: ['count'],
+    beforeRestore: (context) => {
+      console.log('Before hydration...', context)
+    },
+    afterRestore: (context) => {
+      console.log('After hydration...', context.store)
     },
   },
 })
