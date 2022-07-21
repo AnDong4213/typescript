ae2dad162620ddb4714b1e592ebc854b84ad7596
赵 安东 <zhaoandong@bobfintech.com.cn>
2022-07-21 15:15:02 +0800

    pinia 练习

---
+++ again-ts/v-pinia/src/store/todos.js
@@ -0,0 +1,31 @@
+import { defineStore } from 'pinia'
+
+export const useTodos = defineStore('todos', {
+  state: () => ({
+    todos: [],
+    filter: 'all',
+    nextId: 0,
+  }),
+  getters: {
+    finishedTodos(state) {
+      return state.todos.filter((todo) => todo.isFinished)
+    },
+    unfinishedTodos(state) {
+      return state.todos.filter((todo) => !todo.isFinished)
+    },
+    filteredTodos(state) {
+      console.log('state', state)
+      if (this.filter === 'finished') {
+        return this.finishedTodos
+      } else if (this.filter === 'unfinished') {
+        return this.unfinishedTodos
+      }
+      return this.todos
+    },
+  },
+  actions: {
+    addTodo(text) {
+      this.todos.push({ text, id: this.nextId++, isFinished: false })
+    },
+  },
+})
--- again-ts/v-pinia/src/views/AboutView.vue
+++ again-ts/v-pinia/src/views/AboutView.vue
@@ -1,5 +1,21 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="h1" @click="add">This is an about page</h1>
   </div>
 </template>
+
+<script setup>
+import { useCounterStore } from '@/store/counter'
+const counter = useCounterStore()
+
+const add = () => {
+  console.log(99)
+  counter.increment()
+}
+</script>
+
+<style scoped lang="scss">
+.h1 {
+  cursor: pointer;
+}
+</style>
