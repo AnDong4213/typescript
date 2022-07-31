aef64222e0833111f5fe1675ad4b6355fcad1e31
赵安东 <zhaoandong4213@sina.com>
2022-07-31 20:43:47 +0800

    dd

--- again-ts/9-advanced-type/src/9-1.ts
+++ again-ts/9-advanced-type/src/9-1.ts
@@ -1,2 +1,14 @@
-let countq: number = 66911;
-console.log(countq);
+//（1）infer 出现在 extends 条件语句后的函数类型的参数类型位置上
+
+// 仅条件类型的 "extends" 子句中才允许 "infer" 声明
+interface Customer {
+    custname: string
+    buymoney: number
+  }
+
+  type custFuncType  = (cust: Customer,str: string) => string
+
+//   type interType = (params: infer P) => any // 写法是错误的...
+  type inferType<T> = T extends (params: infer P) => any ? P : T
+
+  type inferResultType = inferType<custFuncType>
