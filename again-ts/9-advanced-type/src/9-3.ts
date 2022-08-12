d2d56abddb86a8f88bef355fb6411ea94027e408
赵 安东 <zhaoandong@bobfintech.com.cn>
2022-08-12 09:57:47 +0800

    fix: 格式化

--- again-ts/9-advanced-type/src/9-3.ts
+++ again-ts/9-advanced-type/src/9-3.ts
@@ -1,28 +1,28 @@
-class ChinesePeople  {
-  public name: string
-  public sex: string
-  public phone: string
-  public national: string
+class ChinesePeople {
+  public name: string;
+  public sex: string;
+  public phone: string;
+  public national: string;
 
   constructor(name: string, sex: string, phone: string, national: string) {
     this.name = name;
-    this.sex = sex
-    this.phone = phone
-    this.national = national
+    this.sex = sex;
+    this.phone = phone;
+    this.national = national;
   }
 }
 
 // 一个类的构造函数的类型表示
-const MyChinesePeople: typeof ChinesePeople = ChinesePeople
-const c1 = new MyChinesePeople('安安','男','123','汉');
-console.log(c1)
+const MyChinesePeople: typeof ChinesePeople = ChinesePeople;
+const c1 = new MyChinesePeople("安安", "男", "123", "汉");
+console.log(c1);
 
 // 通用写法
-const MyChinesePeople2: new (...args: any[]) => any = ChinesePeople
-const c2 = new MyChinesePeople2('安安2','男2','1232','汉2');
-console.log(c2)
+const MyChinesePeople2: new (...args: any[]) => any = ChinesePeople;
+const c2 = new MyChinesePeople2("安安2", "男2", "1232", "汉2");
+console.log(c2);
 
 type Constructor<T> = new (...args: any[]) => T;
-const MyChinesePeople3: Constructor<ChinesePeople> = ChinesePeople
-const c3 = new MyChinesePeople2('安安23','男23','12323','汉23');
-console.log(c3 instanceof ChinesePeople)
+const MyChinesePeople3: Constructor<ChinesePeople> = ChinesePeople;
+const c3 = new MyChinesePeople2("安安23", "男23", "12323", "汉23");
+console.log(c3 instanceof ChinesePeople);
--- again-ts/9-advanced-type/src/9-5.ts
+++ again-ts/9-advanced-type/src/9-5.ts
@@ -1,17 +1,21 @@
-class TestClass  {
-  public name: string
-  public classno: number
-  constructor(name: string, classno: number) {
+class TestClass {
+  public name: string;
+  public classNo: number;
+  constructor(name: string, classNo: number) {
     this.name = name;
-    this.classno = classno
+    this.classNo = classNo;
   }
   eat() {
     console.log("姓名为: " + this.name);
   }
 }
 
-type ConstructorParametersType<T extends new (...args: any[]) => any> = T extends new (...args: infer P) => any ? P : never
+type ConstructorParametersType<T extends new (...args: any[]) => any> =
+  T extends new (...args: infer P) => any ? P : never;
 
-let constructorParameters: ConstructorParametersType<typeof TestClass> = ['1', 2]
+let constructorParameters: ConstructorParametersType<typeof TestClass> = [
+  "1",
+  2
+];
 
-console.log(constructorParameters)
+console.log(constructorParameters);
