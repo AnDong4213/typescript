85b0a9265d63c516d3b806c45d5d57f8165dfc25
赵安东 <zhaoandong4213@sina.com>
2022-08-13 10:38:21 +0800

    feat: infer adv

--- again-ts/9-advanced-type/src/9-5.ts
+++ again-ts/9-advanced-type/src/9-5.ts
@@ -6,9 +6,11 @@ class TestClass {
     this.classNo = classNo;
   }
   eat() {
-    console.log("姓名为: " + this.name);
+    console.log("姓名: " + this.name + "，班级：" + this.classNo);
   }
 }
+
+type Constructor<T> = new (...args: any[]) => T;
 
 type ConstructorParametersType<T extends new (...args: any[]) => any> =
   T extends new (...args: infer P) => any ? P : never;
@@ -17,5 +19,20 @@ let constructorParameters: ConstructorParametersType<typeof TestClass> = [
   "1",
   2
 ];
-
 console.log(constructorParameters);
+console.log('------------------------------------------------');
+
+/* function createInstance<T>(constructor: Constructor<T>, ...args: any[]) {
+  // return new constructor(args[0], args[1])
+  return new constructor(...args)
+}
+createInstance<TestClass>(TestClass, "wangwu", 115,).eat(); */
+
+function createInstance<T, CP extends new (...args: any[]) => any>(constructor: Constructor<T>, ...args: ConstructorParametersType<CP>) {
+  return new constructor(...args)
+}
+createInstance<TestClass, typeof TestClass>(TestClass, "wangwu", 151930).eat();
+
+// type LeftTrim<T extends string> = T extends ` ${infer P}` ? LeftTrim<P> : T
+
+export {}
