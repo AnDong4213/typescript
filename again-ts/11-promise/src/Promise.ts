5fb9fe905025233e99bc40b6315b65772f79c665
赵 安东 <zhaoandong@bobfintech.com.cn>
2023-01-07 17:17:07 +0800

    fix: 异步

--- again-ts/11-promise/src/Promise.ts
+++ again-ts/11-promise/src/Promise.ts
@@ -13,14 +13,11 @@ export default class Promise<T = any> {
   public onRejectCallbacks: (() => void)[] = [];
 
   constructor(executor: ExecutorType<T>) {
-    console.log("99999");
     this.status = "pending";
     this.resolve = (value: any): any => {
       if (this.status === "pending") {
-        console.log("onResolveCallbacks---", this.onResolveCallbacks);
         this.status = "success";
         this.resolve_success_value = value;
-        console.log("resolve函数的结果:", value);
         this.onResolveCallbacks.forEach((callback) => callback());
       }
     };
@@ -29,12 +26,10 @@ export default class Promise<T = any> {
       if (this.status === "pending") {
         this.status = "fail";
         this.reject_fail_value = reason;
-        console.log("reject函数的结果:", reason);
       }
     };
 
     try {
-      // console.log("执行次数---");
       executor(this.resolve, this.reject);
     } catch (err: any) {
       this.status = "pending";
@@ -46,33 +41,39 @@ export default class Promise<T = any> {
   then(resolveInThen: ResolveType, rejectinThen: FailType) {
     return new Promise((resolve, reject) => {
       let result;
-      console.log("this.status", this.status);
       if (this.status === "success") {
         result = resolveInThen(this.resolve_success_value);
         resolve(result);
-        console.log("resolveInThen--执行");
       }
 
       if (this.status === "fail") {
         result = rejectinThen(this.reject_fail_value);
         reject(result);
-        console.log("rejectinThen--执行");
       }
 
       if (this.status === "pending") {
-        console.log("异步的then---");
         this.onResolveCallbacks.push(() => {
           result = resolveInThen(this.resolve_success_value);
-          console.log("订阅函数resolve执行---", result);
           resolve(result);
         });
 
         this.onRejectCallbacks.push(() => {
           result = rejectinThen(this.reject_fail_value);
-          console.log("订阅函数reject执行---", result);
           reject(result);
         });
       }
     });
   }
+}
+
+function isPromise(val: any): val is Promise {
+  return isObject(val) && isFunction(val.then);
+}
+
+function isObject(val: any): val is Record<any, any> {
+  return val !== null && typeof val === "object";
+}
+
+function isFunction(data: any): data is Function {
+  return typeof data === "function";
 }
--- again-ts/11-promise/src/index.ts
+++ again-ts/11-promise/src/index.ts
@@ -1,4 +1,4 @@
-import Promise from "./Promise";
+import Promise from "./Promise-2";
 
 const promise = new Promise((resolve, reject) => {
   // resolve("成功了...");
@@ -18,11 +18,44 @@ const promise = new Promise((resolve, reject) => {
   }
 ); */
 
+/* promise
+  .then(
+    (successinfo) => {
+      console.log("successinfo-1:", successinfo);
+      return "OK-1";
+    },
+    (errorinfo) => {
+      console.log("errorinfo-1:", errorinfo);
+      return "fail-1";
+    }
+  )
+  .then(
+    (successinfo2) => {
+      console.log("successinfo-2:", successinfo2);
+      return "OK-2";
+    },
+    (errorinfo) => {
+      console.log("errorinfo:-2", errorinfo);
+      return "fail-2";
+    }
+  )
+  .then(
+    (successinfo3) => {
+      console.log("successinfo-3:", successinfo3);
+    },
+    (errorinfo) => {
+      console.log("errorinfo:-3", errorinfo);
+    }
+  ); */
+
 promise
   .then(
     (successinfo) => {
       console.log("successinfo-1:", successinfo);
       return "OK-1";
+      /* return new Promise((resolve, reject) => {
+        resolve("第二个异步操作。。。。");
+      }); */
     },
     (errorinfo) => {
       console.log("errorinfo-1:", errorinfo);
