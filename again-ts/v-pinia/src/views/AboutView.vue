c35252b48a43e4138fadc82cd1b01897037f98a1
赵安东 <zhaoandong4213@sina.com>
2023-10-27 16:00:52 +0800

    fix: vue

--- again-ts/v-pinia/src/views/AboutView.vue
+++ again-ts/v-pinia/src/views/AboutView.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="about">
-    <h1 class="h1" @click="add">This is an about page</h1>
+    <h1 class="h1"
+        @click="add">This is an about page</h1>
     <h2 @click="action">{{ counter.name }}--{{ counter.count }}</h2>
+
+    <div style="margin: 30px;background-color: beige;">
+      <div class="nav"></div>
+      <div class="nav navActive"></div>
+      <div class="nav"></div>
+    </div>
   </div>
 </template>
 
@@ -30,5 +37,40 @@ onMounted(() => {
 <style scoped lang="scss">
 .h1 {
   cursor: pointer;
+}
+
+.nav {
+  height: 60px;
+}
+
+.navActive {
+  background-color: #fff;
+  color: rgb(0, 63, 136);
+  position: relative;
+}
+
+.navActive::before,
+.navActive::after {
+  content: '';
+  display: block;
+  height: 8px;
+  width: 8px;
+  position: absolute;
+  bottom: 0;
+  background: radial-gradient(6px at 6px 0px,
+      transparent 6px,
+      #fff 6px);
+}
+
+.navActive::before {
+  right: 0;
+  bottom: calc(-1 * 6px);
+  transform: scale(-1);
+}
+
+.navActive::after {
+  right: 0;
+  top: calc(-1 * 6px);
+  transform: scaleX(-1);
 }
 </style>
